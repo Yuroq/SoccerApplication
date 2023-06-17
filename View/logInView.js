ff42515626bd84ef26121c4d5e26caceb92ebdc3
Jamal Kayed <119996570+Yuroq@users.noreply.github.com>
2023-06-17 16:31:11 -0400

    Update logInView.js

--- View/logInView.js
+++ View/logInView.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username')
 const password = document.querySelector('#password')
 const login = document.querySelector('.Log-in-Button')
-
+document.body.style.zoom="67%"
 const user = 'Soccer'
 const pass = 'Football'
 
@@ -9,7 +9,7 @@ login.addEventListener('click', function(e) {
 if(username.value == user && password.value == pass) {
     console.log('✅');
     e.preventDefault()
-    location.href = "index.html";
+    location.href = "Home.html";
 
 } else {
     alert('Invalid log in info❌')
